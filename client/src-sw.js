3da5879a2eec783b3287bc343cd9cb268f223dd9
McCoy Didericksen <mccoy.did@gmail.com>
2023-03-01 20:17:13 -0700

    snapshot while debugging

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -7,24 +7,24 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
+// const pageCache = new CacheFirst({
+//   cacheName: 'page-cache',
+//   plugins: [
+//     new CacheableResponsePlugin({
+//       statuses: [0, 200],
+//     }),
+//     new ExpirationPlugin({
+//       maxAgeSeconds: 30 * 24 * 60 * 60,
+//     }),
+//   ],
+// });
 
-warmStrategyCache({
-  urls: ['/index.html', '/'],
-  strategy: pageCache,
-});
+// warmStrategyCache({
+//   urls: ['/index.html', '/'],
+//   strategy: pageCache,
+// });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 registerRoute(
   ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,17 +12,16 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
+    console.log('content', content);
     const contactDb = await openDB('jate', 1);
     const tx = contactDb.transaction('jate', 'readwrite');
     const store = tx.objectStore('jate');
-    const request = store.put(content);
+    const request = store.put({id: 1, content: content});
     const result = await request;
     return result;
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
     const contactDb = await openDB('jate', 1)
     const tx = contactDb.transaction('jate', 'readonly');
